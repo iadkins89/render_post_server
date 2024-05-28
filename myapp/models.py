38f28ad9b2ac61cb44ea401a275f71ac4acdc133
Ian Adkins <ian.adkins1989@gmail.com>
2024-05-28 18:00:07 -0500

    try

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from .extensions import db 
 
-class data(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class TelemetryData(db.Model):
+    timestamp = db.Column(db.timestamp, primary_key=True)
     rssi = db.Column(db.Float)
     snr = db.Column(db.Float)
     temperature = db.Column(db.Float)
